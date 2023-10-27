e4da1f44994e2b008972845d1cd3a23c856690a1
Pac12Run8000 <gangstarr78992@>
2023-10-27 10:11:45 -0700

    Initial Commit

---
+++ ContentSizeCategory/ContentSizeCategoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentSizeCategoryApp.swift
+//  ContentSizeCategory
+//
+//  Created by Michelle Grover on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ContentSizeCategoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentSizeCategory/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ContentSizeCategory
+//
+//  Created by Michelle Grover on 10/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
